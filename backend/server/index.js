4ba3ee3826d6d08759acd8ba38ef47c765b28803
vishnu <vishnu.vardhan200406@gmail.com>
2026-04-24 06:41:44 +0530

    fix app not defined error

--- backend/server/index.js
+++ backend/server/index.js
@@ -17,7 +17,6 @@ import facultyRoutes from './routes/facultyRoutes.js';
 connectDB();
 
 const app = express();
-export default app;
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -51,3 +50,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('? Server started on port ' + PORT);
 });
+export default app;
